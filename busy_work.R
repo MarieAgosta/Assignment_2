bc06e5cb0ed1ab5ce65cd920072b6aef6b3e1a0e
Marie Agosta <M.Agosta@lse.ac.uk>
2016-10-25 16:57:03 +0200

    Input the lit review and first bibtex
    Will finish the rest by Wednesday

---
+++ busy_work.R
@@ -0,0 +1,22 @@
+
+install.packages("readstata13")
+
+library(readstata13)
+
+
+install.packages("foreign")
+
+library(foreign)
+
+read.dta("anes_timeseries_2012_Stata12.dta")
+
+getwd()
+
+
+file.choose("anes_timeseries_2012.sav")
+
+dataset = read.spss("/Users/Marie/Desktop/R_Course /Assignment_2/anes_timeseries_2012.sav", to.data.frame=TRUE)
+
+Aneas_db = file.choose("/Users/Marie/Desktop/R_Course /Assignment_2/anes_timeseries_2012.sav")
+
+
